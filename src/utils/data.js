91b993014c277aa7704d2166c9ffb79eecdf39e1
SChandana21 <chanduvalluri22@gmail.com>
2024-03-02 12:23:13 +0530

    before setting data

---
+++ src/utils/data.js
@@ -0,0 +1,27 @@
+import d4 from '../img/d4.png'
+import fi5 from '../img/fi5.png'
+import r4 from '../img/r4.png'
+
+
+export const heropData = [
+  {
+    id: 1,
+     name: 'Mocktail',
+      decp: 'Virgin Blue Hawaiian Mocktail',
+       price: '5.25',
+        imageSrc : d4
+      },
+
+  {id: 2,
+     name: 'Main course',
+      decp: 'Fish Curry',
+       price: '6.25', 
+       imageSrc : fi5
+      },
+  {id: 3,
+     name: 'Rice',
+      decp: 'Pan tossed Fried rice',
+       price: '3.25',
+        imageSrc : r4
+      },
+]
