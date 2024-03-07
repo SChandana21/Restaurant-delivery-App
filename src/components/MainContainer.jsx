a7f448f3cbcdbdc9dcd1d71a9cd43add34764102
SChandana21 <chanduvalluri22@gmail.com>
2024-03-07 15:38:21 +0530

    6:10

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -21,8 +21,8 @@ return (
         <p className='text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600  transition ease-in-out duration-100'>Our Bestsellers
         </p>
         <div className='hidden md:flex gap-3 items-center'>
-          <motion.div  whileTap={{scale: 0.75}} className='w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center'
-          onClick={() => setScrollValue(-200)}>
+          <motion.div  whileTap={{scale: 0.75}} className='w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer  hover:shadow-lg flex items-center justify-center'
+          onClick={() => setScrollValue(-200)}> 
             <MdChevronLeft className='text-base text-white' />
 
           </motion.div>
