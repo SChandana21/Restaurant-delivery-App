a14e98b9057caab1cccfd253140c9a3ba97c2691
SChandana21 <chanduvalluri22@gmail.com>
2024-03-07 13:56:28 +0530

    Working fine

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -20,16 +20,16 @@ const RowContainer = ({flag, data, scrollValue }) => {
        {data && data.map(item =>(
          <div className='w-full min-w-[300px] md:min-w-[340px] md:w-340 h-auto bg-cardOverlay rounded-lg p-4  backdrop-blur-lg hover:drop-shadow-lg mb-2 '>
          <div className='w-full flex items-center justify-between '>
-           <motion.img whileHover={{scale: 1.2 }}src= "https://firebasestorage.googleapis.com/v0/b/restaurantapp-c4f0e.appspot.com/o/Images%2F1709628932720-i3.png?alt=media&token=d103b7f0-3028-4760-888f-3d9807bf15f3" alt='icecream' className='w-20 flex' />
+           <motion.img whileHover={{scale: 1.2 }}src= {item?.imageURL} alt='icecream' className='w-20 flex' />
            <motion.div onTap={{scale: 0.75}} className='w-8 h-8 rounded-full mb-16 bg-orange-400 flex items-center justify-center cursor-pointer hover:shadow-md'>
-             < MdShoppingBasket className='text-white ' />
+             < MdShoppingBasket className='text-white ' /> 
            </motion.div>
          </div>
          <div className='w-full flex items-start justify-start gap-2'>
-           <p className='text-textColor font-semibold text-base md:text-lg mr-10 '>Vanilla Cone</p>
+           <p className='text-textColor font-semibold text-base md:text-lg mr-10 '>{item?.title}</p>
            <p className='text-lg text-headingColor font-semibold flex justify-start'> 
            <p className='mt-16 mr-24 text-sm text-gray-500 absolute top-2 right-4'></p>
-           <span className=' text-imagecolor ml-16'>$</span> 5.25
+           <span className=' text-imagecolor ml-16'>$</span> {item?.price}
            </p>
          </div>
              </div>
