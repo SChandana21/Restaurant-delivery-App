3f141374f38518a32904d82dfae911a2d9aabc1b
SChandana21 <chanduvalluri22@gmail.com>
2024-03-07 21:08:08 +0530

    workingfineig

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { MdOutlineKeyboardBackspace } from 'react-icons/md';
 import { motion} from 'framer-motion';
 import {RiRefreshFill} from 'react-icons/ri';
-import { BiMinus } from "react-icons/bi";
-import { BiPlus } from "react-icons/bi";
+
 import { useStateValue } from '../context/StateProvider';
 import { actionType } from '../context/reducer';
 import EmptyCart from '../img/emptyCart.svg'
+import CartItem from './CartItem';
 
 
 
@@ -35,30 +35,10 @@ const CartContainer = () => {
       </div>
       {/* bottom section */}
       <div className='w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col'>
-        <div key={cartItems.id} className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none'>
+        <div  className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none'>
           {/* cart item */}
           {cartItems && cartItems.map(item => (
-            <div className='w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2'>
-            <img src={item?.imageURL} alt='rice'className='w-20 h-20 max-w-[60px] rounded-full object-contain' />
-            {/* Name */}
-            <div className='flex flex-col gap-2'>
-              <p className='text-base text-gray-50'>{item?.title}</p>
-              <p className='text-sm block text-gray-300 font-semibold'>{item?.price}</p>
-            </div>
-            <div className='group flex items-center gap-2 ml-auto cursor-pointer'>
-              <motion.div whileTap={{scale: 0.75}}>
-                <BiMinus className='text-gray-50'/>
-              </motion.div>
-              <p className='w-5 h-5 rounded-sm bg-cartBg  text-gray-50 flex items-center justify-center'>
-                {item.qty}
-              </p>
-              <motion.div whileTap={{scale: 0.75}}>
-              <BiPlus className='text-gray-50'/>
-                
-              </motion.div>
-            </div>
-
-          </div>
+            <CartItem key={cartItems.id} item={item} />
           ))}
         </div>
 
